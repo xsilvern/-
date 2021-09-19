2f6d63ad76c9c698c15af77df5cb93103fba67e9
20213020신동은 <50288644+xsilvern@users.noreply.github.com>
2021-09-19 20:06:25 +0900

    Add files via upload

---
+++ 04_example_3.ino
@@ -0,0 +1,30 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED,OUTPUT);
+  Serial.begin(115200);
+  while(!Serial){
+    ;
+  }
+  Serial.println("Hello World!");
+  count = toggle = 0;
+  digitalWrite(PIN_LED, toggle);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(++count);
+  toggle=toggle_state(toggle);
+  digitalWrite(PIN_LED,toggle);
+  delay(1000);
+}
+int toggle_state(int toggle){
+  if(toggle==1){
+    toggle=0;
+  }
+  else{
+    toggle=1;
+  }
+  return toggle;
+}
